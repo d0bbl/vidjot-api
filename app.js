a5d1bc05fd515ee56f4d45a7aca56c8122b03a0d
dobbleXg <kennybajomo@gmail.com>
2021-02-18 13:55:05 +0100

    enacting best practices 1

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 const express = require("express");
 const morgan = require("morgan");
+require("dotenv").config({ debug: process.env.DEBUG });
 const cors = require("cors");
 const mongoose = require("mongoose")
-const db = require("./config/database");
 const IdeaRoutes = require("./base/routes/idea/idea.route");
 const UserRoutes = require("./base/routes/user/user.route");
 
 const app = express();
+// const dotenv = require("dotenv");
+// const {MONGO_URI} = process.env;
+// const buf = Buffer.from("MONGO_URI");
+// const opt = { debug: true };
+// const config = dotenv.parse(buf, opt);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 
+// console.log(`${process.env.PORT}`);
 // connect to mongoDB
 (async() => {
   try {
 
-    await mongoose.connect(db.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true , useCreateIndex: true, useFindAndModify: false });
+    const connectmDB = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true , useCreateIndex: true, useFindAndModify: false });
+
+    if(connectmDB) {
+    console.log(`Connected to DB`);
+    }
 
   } catch (err) {
     console.log(`Failed to connect to DB ${err}`);
-  } finally {
-    console.log('Connected to DB ');
   }
 })();
 
